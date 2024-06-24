1539e0d99ef2e4b71d1b099a782ee7b142a030b8
Aayush Rai <coolaayushrai@gmail.com>
2024-06-24 22:36:07 +0530

    Add priority property to Task object

--- src/object-constructors.js
+++ src/object-constructors.js
@@ -1,9 +1,10 @@
 const taskList = [];
 
-const Task = function(name, description, dueDate, idGen) {
+const Task = function(name, description, dueDate, priority, idGen) {
     this.name = name;
     this.description = description;
     this.dueDate = dueDate;
+    this.priority = priority;
     this.id = idGen;
 };
 
