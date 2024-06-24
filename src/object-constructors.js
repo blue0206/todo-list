fa193dc8ce9bdbe1aa894aeeb6393f8eec8e870c
Aayush Rai <coolaayushrai@gmail.com>
2024-06-24 22:58:52 +0530

    Add status property to task object with false as default value
    The false value corresponds to "Task Incomplete" while true value
    corresponds to "Task Complete".

--- src/object-constructors.js
+++ src/object-constructors.js
@@ -1,11 +1,13 @@
 const taskList = [];
 
-const Task = function(name, description, dueDate, priority, idGen) {
+const Task = function(name, description, dueDate, priority, idGen, status=false) {
     this.name = name;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.id = idGen;
+    // Status = false corresponds to "Task Incomplete" and vice-versa.
+    this.status = status;
 };
 
 export { Task, taskList };
