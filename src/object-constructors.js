2b1eedb7cffe65d2e36b7fa75b800c64cd80659a
Aayush Rai <coolaayushrai@gmail.com>
2024-06-26 21:05:08 +0530

    Add project property to task object constructor function
    The property stores the name of the project to which the task
    belongs.

--- src/object-constructors.js
+++ src/object-constructors.js
@@ -1,12 +1,13 @@
 const taskList = [];
 const projectList = [];
 
-const Task = function(name, description, dueDate, priority, id, status=false) {
+const Task = function(name, description, dueDate, priority, id, project, status=false) {
     this.name = name;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
     this.id = id;
+    this.project = project;
     // Status = false corresponds to "Task Incomplete" and vice-versa.
     this.status = status;
 };
