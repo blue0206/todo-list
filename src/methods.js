64d04e5c5ac09c314ce42f63e11c3fafadaf0521
Aayush Rai <coolaayushrai@gmail.com>
2024-06-27 21:19:37 +0530

    Refactor listMethod search & remove code
    Both search and remove methods made use of binary search. Therefore,
    binary search algorithm has been created seperately serving as the
    common go-to function for both the methods that use it.
    
    This saves us from writing the same binary search code twice. We
    just take in a boolean serving as whether we want to search or we
    want to remove and perform the operation accordingly.

--- src/methods.js
+++ src/methods.js
@@ -2,18 +2,30 @@ const listMethods = (list) => {
     const add = (item) => list.push(item);
 
     const remove = (itemID) => {
+        binarySearch(itemID, false);
+    };
+
+	const search = (itemID) => {
+		return binarySearch(itemID);
+	};
+
+    const binarySearch = (target, search=true) => {
         let start = 0;
         let end = list.length - 1;
         let mid = Math.floor(start + (end-start)/2);
 
         while (start <= end)
         {
-            if (list[mid].id == itemID)
+            if (list[mid].id == target)
             {
+                if (search)
+                {
+                    return list[mid];
+                }
                 list.splice(mid, 1);
                 break;
             }
-            else if (list[mid].id < itemID)
+            else if (list[mid].id < target)
             {
                 start = mid + 1;
             }
@@ -24,29 +36,6 @@ const listMethods = (list) => {
             mid = Math.floor(start + (end-start)/2);
         }
     };
-
-	const search = (itemID) => {
-		let start = 0;
-        let end = list.length - 1;
-        let mid = Math.floor(start + (end-start)/2);
-
-        while (start <= end)
-        {
-            if (list[mid].id == itemID)
-            {
-                return list[mid];
-            }
-            else if (list[mid].id < itemID)
-            {
-                start = mid + 1;
-            }
-            else
-            {
-                end = mid - 1;
-            }
-            mid = Math.floor(start + (end-start)/2);
-        }
-	};
 
     return { add, remove, search };
 };
