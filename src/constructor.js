b6699f0c2b51afacdc654d50543992aac1c80644
Aayush Rai <coolaayushrai@gmail.com>
2024-07-16 18:28:33 +0530

    Add functionality to edit tasks
    The taskEditor() function of TaskControl IIFE handles the complete
    process of editing of an existing task.
    
    Proper comments have been added to clearly explain what the code
    does.
    
    It has been placed alongside the taskConstructor() function which
    handles the creation of new tasks. Hence, both serve as part of
    TaskControl IIFE.

--- src/constructor.js
+++ src/constructor.js
@@ -1,6 +1,7 @@
 import { Task, TaskList, Project, ProjectList } from "./object-constructors.js";
 import { ProjectDOM, ProjectListDOM } from "./dom-object-constructors.js";
-import { dropDownListMethods, displayControl } from "./methods.js";
+import { dropDownListMethods, displayControl, listMethods } from "./methods.js";
+import { TaskDisplayControl } from "./display.js";
 import { format } from "date-fns";
 
 let idGen = 1;
@@ -56,6 +57,87 @@ const TaskControl = function() {
         });
     };
 
+    function taskEditor()
+    {
+        const taskWindow = document.querySelector(".task-modal");           // Task Display Modal
+        const taskEditModal = document.querySelector(".edit-task-modal");   // Task Edit Modal
+        const editModalBtn = taskWindow.querySelector(".edit-task");        // Modal button
+        const editSubmitBtn = taskEditModal.querySelector(".modify-btn");
+        const editCancelBtn = taskEditModal.querySelector(".cancel-btn");
+        // Create parent-project drop-down handler.
+        const taskDropDown = dropDownListMethods(
+            taskEditModal.querySelector('#parent-project')
+        );
+        let task = null;    // Will store the task to be edited.
+
+        // Task edit modal button event listener to display modal on click.
+        editModalBtn.addEventListener('click', () => {
+            taskWindow.close();         // Close task display modal.
+            taskEditModal.showModal();  // Show task edit modal
+            task = TaskList.search(editModalBtn.id);    // Store the task to be edited.
+            // Setup the task edit fields with existing data.
+            setupTaskEditFields(task);
+        });
+
+        // Submit button event listener.
+        editSubmitBtn.addEventListener('click', () => {
+            // Get rid of generated project list.
+            taskDropDown.remove();
+
+            // Update the task with new data input by user using update method.
+            task.update(
+                taskEditModal.querySelector('#task-name').value,
+                taskEditModal.querySelector('#description').value,
+                taskEditModal.querySelector('#due-date').value,
+                taskEditModal.querySelector('#priority').value,
+                task.id,
+                task.project,
+                task.status
+            );
+            
+            taskEditModal.close();      // Close task edit modal upon submit.
+
+            TaskDisplayControl.taskDisplay(task);   // Display the task window after update.
+
+            if (task.project == 0)  // If task is present in inbox.
+            {
+                document.querySelector('button.inbox').dispatchEvent(new MouseEvent('click'));
+            }
+            else    // If task is not present in inbox but another project.
+            {
+                const projects = Array.from(document.querySelectorAll('.project-tabs > .project-item'));
+                listMethods(projects).search(task.project).dispatchEvent(new MouseEvent(
+                    'click', 
+                    // Set to true as the event listener has been set up for parent element of node.
+                    { bubbles: true }
+                ));
+            }
+        });
+
+        // Cancel button event listener.
+        editCancelBtn.addEventListener('click', () => {
+            // Get rid of generated project list.
+            taskDropDown.remove();
+
+            taskEditModal.close();  // Close task edit modal upon cancelling.
+            taskModal.showModal();  // Display the task.
+        });
+
+        function setupTaskEditFields(task)
+        {
+            taskEditModal.querySelector('#task-name').value = task.name;
+            taskEditModal.querySelector('#description').value = task.description;
+            taskEditModal.querySelector('#due-date').value = task.dueDate;
+            taskEditModal.querySelector('#priority').value = task.priority;
+            
+            // Generate project list.
+            taskDropDown.generate(ProjectList.list);
+            // Set current parent project as selected in the drop-down.
+            const parentProjectSelection = taskEditModal.querySelector('#parent-project');
+            parentProjectSelection.querySelector(`option[value="${task.project}"]`).toggleAttribute('selected');
+        }
+    }
+
     function taskSetup(name, description, dueDate, priority, parentProject)
     {
         // APPLICATION-SIDE
@@ -69,7 +151,7 @@ const TaskControl = function() {
             idGen++,            // Increment ID generator.
             parentProject.value
         );
-        
+
         // Push created task into global and parent project task list.
         TaskList.add(task);
         ProjectList.search(task.project).add(task);
@@ -84,10 +166,10 @@ const TaskControl = function() {
         displayControl.updateParentProjectDisplay(projectDOM, task);
 
         // Display task upon creation.
-        displayControl.taskDisplay(task);
+        TaskDisplayControl.taskDisplay(task);
     }
 
-    return { taskConstructor };
+    return { taskConstructor, taskEditor };
 }();
 
 // Project Constructor
