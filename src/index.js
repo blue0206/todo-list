160cbd27ca15996cb935b1659dd253039d11b1bc
Aayush Rai <coolaayushrai@gmail.com>
2024-07-20 17:17:34 +0530

    Impirt & call myProjectsTab() to setup display for "My Projects"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import "./style.css";
 import { TaskControl, ProjectControl } from "./constructor.js";
-import { ProjectDisplayControl } from "./display.js";
+import { ProjectDisplayControl, myProjectsTab } from "./display.js";
 
 TaskControl.taskConstructor();
 TaskControl.taskEditor();
 ProjectControl.projectConstructor();
 ProjectDisplayControl.inboxDisplay();
 ProjectDisplayControl.projectDisplay();
+myProjectsTab();
