a1a2a59fd04c79084c3b812ce03655fb597acc94
Aayush Rai <coolaayushrai@gmail.com>
2024-07-22 23:21:33 +0530

    Call taskDestructor() method to enable task deletion

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { ProjectDisplayControl, myProjectsTab } from "./display.js";
 
 TaskControl.taskConstructor();
 TaskControl.taskEditor();
+TaskControl.taskDestructor();
 ProjectControl.projectConstructor();
 ProjectDisplayControl.inboxDisplay();
 ProjectDisplayControl.projectDisplay();
