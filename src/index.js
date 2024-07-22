fc3c3e5099c2e0c2b493a10a6af45f0a6de1b56b
Aayush Rai <coolaayushrai@gmail.com>
2024-07-22 23:56:58 +0530

    Remove deprecated IIFE method calls; setup inbox using inboxSetup()
    ProjectDisplayControl IIFE has been discarded and hence, its calls
    are no longer needed.
    
    The inboxSetup function initializes the inbox and attaches the
    required event listener to its sidebar button.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import "./style.css";
-import { TaskControl, ProjectControl } from "./constructor.js";
-import { ProjectDisplayControl, myProjectsTab } from "./display.js";
+import { TaskControl, ProjectControl, inboxSetup } from "./constructor.js";
+import { myProjectsTab } from "./display.js";
 
 TaskControl.taskConstructor();
 TaskControl.taskEditor();
 TaskControl.taskDestructor();
 ProjectControl.projectConstructor();
-ProjectDisplayControl.inboxDisplay();
-ProjectDisplayControl.projectDisplay();
+inboxSetup();
 myProjectsTab();
