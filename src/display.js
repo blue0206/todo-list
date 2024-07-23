08532f9cc2857d09d60c3b768e3fcf3005ab7840
Aayush Rai <coolaayushrai@gmail.com>
2024-07-23 21:37:49 +0530

    Refactor refreshDisplay() function & update conditionals
    The conditional which checked whether a deleted project is being
    displayed has been moved inside the conditional dealing with
    projects. That place is better suited as now the conditional is
    placed alongside the conditionals handling the display of projects.
    
    The primary conditionals that checked whether the "My Projects" tab
    is being displayed or a project is being displayed has been updated
    with checks for whether the mainChild node (the direct child node of
    main element section) is valid or not.
    This has been done because when the child node of main section is
    deleted in case of the display of a deleted project, it lead to
    conflicts when the refreshDisplay() function was further called as
    the mainChild node now had the value 'null' which gave errors
    when the code in conditionals was executed.
    Hence the changes made provide us with a safety-check and prevent
    any arise of errors.

--- src/display.js
+++ src/display.js
@@ -48,17 +48,17 @@ function refreshDisplay(projectID=null, sidebarDisplay=false)
 {
     const mainChild = document.querySelector('main').lastChild;
 
-    if (mainChild.className == "my-projects")   // Refresh "My Projects" tab if on display.
+    if (mainChild && mainChild.className == "my-projects")   // Refresh "My Projects" tab if on display.
     {
         document.querySelector('.project-tabs .my-projects').dispatchEvent(new MouseEvent('click'));
     }
-    else if (projectID !== null && !ProjectListDOM.search(projectID))  // If ID provided & project is deleted.
+    else if (mainChild && mainChild.id == projectID && projectID !== null)  // Refresh project tab if on display & id provided.
     {
-        document.querySelector('main').removeChild(mainChild);         // Remove deleted project from display.
-    }
-    else if (mainChild.id == projectID && projectID !== null)  // Refresh project tab if on display & id provided.
-    {
-        if (projectID == 0)     // ID == 0 refers to Inbox.
+        if (!ProjectListDOM.search(projectID))  // If ID provided but project is deleted.
+        {
+            document.querySelector('main').removeChild(mainChild);         // Remove deleted project from display.
+        }
+        else if (projectID == 0)     // ID == 0 refers to Inbox.
         {
             document.querySelector('button.inbox').dispatchEvent(new MouseEvent('click'));
         }
