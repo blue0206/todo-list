cc4fc767d13978d1953de515bb726c91b7e6bf4a
Aayush Rai <coolaayushrai@gmail.com>
2024-07-25 00:35:03 +0530

    Generate task-card DOM content using taskCard() delegate function
    The taskCard() function generates the card DOM content of each task
    and is called in the TaskList loop in the tasks() function in order
    to generate the task-card of each task.

--- src/home.js
+++ src/home.js
@@ -59,9 +59,82 @@ function tasks()
 
     // Create task card.
     TaskList.list.forEach((task) => {
-        
+        taskCardsContainer.appendChild(taskCard(task));
     });
 
     taskSection.appendChild(taskCardsContainer);
     return taskSection;
+}
+
+function taskCard(task)
+{
+    // Task card container.
+    const taskCard = document.createElement('div');
+    taskCard.classList.add('task-card');
+    taskCard.id = task.id;
+
+    // Task card sideline.
+    const sideline = document.createElement('div');
+    sideline.classList.add(`${task.priority.toLowerCase()}`, 'sideline');
+    taskCard.appendChild(sideline);
+
+    // Task card content.
+    const cardContent = document.createElement('div');
+    cardContent.classList.add('card-content');
+
+    // Task Name
+    const taskName = document.createElement('h3');
+    taskName.classList.add('task-name');
+    taskName.textContent = task.name;
+    cardContent.appendChild(taskName);
+
+    // Task Description
+    const taskDescription = document.createElement('div');
+    taskDescription.classList.add('task-description');
+    taskDescription.textContent = task.description;
+    cardContent.appendChild(taskDescription);
+
+    // Task Due Date
+    const taskDueDate = document.createElement('div');
+    taskDueDate.classList.add('task-due-date');
+    taskDueDate.textContent = `Due Date: ${task.dueDate}`;
+    cardContent.appendChild(taskDueDate);
+
+    // Task card buttons container.
+    const btns = document.createElement('div');
+    btns.classList.add('card-btns');
+
+    // Mark Complete button.
+    const markCompleteBtn = document.createElement('button');
+    markCompleteBtn.classList.add('card-mark-btn');
+    const markCompleteIcon = new Image();
+    markCompleteIcon.src = "";
+    markCompleteIcon.alt = "Mark Complete";
+    markCompleteBtn.appendChild(markCompleteIcon);
+
+    // Edit button.
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('card-edit-btn');
+    const editIcon = new Image();
+    editIcon.src = "";
+    editIcon.alt = "Edit Task";
+    editBtn.appendChild(editIcon);
+
+    // Delete button.
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('card-delete-btn');
+    const deleteIcon = new Image();
+    deleteIcon.src = "";
+    deleteIcon.alt = "Delete Task";
+    deleteBtn.appendChild(deleteIcon);
+
+    // Append buttons to container.
+    btns.appendChild(markCompleteBtn);
+    btns.appendChild(editBtn);
+    btns.appendChild(deleteBtn);
+
+    cardContent.appendChild(btns);
+    taskCard.appendChild(cardContent);
+
+    return taskCard;
 }
