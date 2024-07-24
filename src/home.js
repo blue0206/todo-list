24db9a184342f6736fa96c567356bd83dcf233b2
Aayush Rai <coolaayushrai@gmail.com>
2024-07-25 00:08:28 +0530

    Create default function to generate home tab DOM content

---
+++ src/home.js
@@ -0,0 +1,11 @@
+export default function content()
+{
+    const main = document.querySelector('main');
+    if (main.lastChild)
+    {
+        main.removeChild(main.lastChild);
+    }
+
+    const home = document.createElement('div');
+    home.classList.add('home');    
+}
