29f42ad67a1fbd41b34ca6e2309759f6f71719b2
Aayush Rai <coolaayushrai@gmail.com>
2024-07-25 00:22:46 +0530

    Create delegate function to generate task section of home tab content
    The tasks() function generates the tasks section content for the main
    section of the home tab DOM content.

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import { TaskList } from "./object-constructors";
+
 export default function content()
 {
     const main = document.querySelector('main');
@@ -36,4 +38,30 @@ function mainContent()
     const mainSection = document.createElement('div');
     mainSection.classList.add('home-main');
 
+    // Append tasks section content.
+    mainSection.appendChild(tasks());
+}
+
+function tasks()
+{
+    const taskSection = document.createElement('div');
+    taskSection.classList.add('home-task');
+
+    // Task section heading.
+    const heading = document.createElement('h2');
+    heading.classList.add('section-heading');
+    heading.textContent = "Your Tasks";
+    taskSection.appendChild(heading);
+
+    // Task cards container.
+    const taskCardsContainer = document.createElement('div');
+    taskCardsContainer.classList.add('task-cards');
+
+    // Create task card.
+    TaskList.list.forEach((task) => {
+        
+    });
+
+    taskSection.appendChild(taskCardsContainer);
+    return taskSection;
 }
