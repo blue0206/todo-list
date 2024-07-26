84cd3c0e2ff151414be90a4d1d3a650aac44322f
Aayush Rai <coolaayushrai@gmail.com>
2024-07-26 14:48:16 +0530

    Generate sidebar content using function imported from sidebar module

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import "./style.css";
 import { TaskControl, ProjectControl, inboxSetup } from "./constructor.js";
 import { myProjectsTab, homeTab } from "./display.js";
 import home from "./home.js";
+import sidebar from "./sidebar.js";
 
+sidebar();
 TaskControl.taskConstructor();
 TaskControl.taskEditor();
 TaskControl.taskDestructor();
